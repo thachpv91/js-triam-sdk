3161d140e902c692f12fe8c128d09ca98e9d806e
thuannd <thuannd@bap.jp>
2018-10-31 16:53:39 +0700

    SCInterface

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export {Server} from "./server";
 export {FederationServer, FEDERATION_RESPONSE_MAX_SIZE} from "./federation_server";
 export {StellarTomlResolver, STELLAR_TOML_MAX_SIZE} from "./stellar_toml_resolver";
 export {CoreConnection} from "./core_connection";
+export {SCInterface} from "./sc_interface";
 
 // expose classes and functions from stellar-base
 export * from "stellar-base";
---
+++ src/sc_interface.js
@@ -0,0 +1,27 @@
+/**
+ * Server handles the network connection to a [Stellar-core] (http://localhost:11626)
+ * @constructor
+ */
+export class SCInterface {
+
+    /**
+    * constructor
+    * @returns {Array} args - arguments
+    */
+    constructor(args) {}
+
+    /**
+    * execute a function follow by a function name
+    * @param {string} funcName - function name.
+    * @param {Array} args - arguments
+    */
+   main(funcName, args) {}
+
+    /**
+    * check amount
+    * @param {Array} args - arguments
+    */
+    fallback(args) {}
+
+}
+
